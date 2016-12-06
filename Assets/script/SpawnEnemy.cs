d50ea830221283711a09119de0f34130b1679386
wargreyguy25 <20wkeller@cr.k12.ia.us>
2016-12-06 11:16:17 -0600

    inital commit

---
+++ Assets/script/SpawnEnemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public float spawnTime;        // The amount of time between each spawn.
+	public float spawnDelay;       // The amount of time before spawning starts.
+	public GameObject[] enemies;        // Array of enemy prefabs.
+
+	public int maxDistance;
+	public Transform target;
+	public Transform myTransform;
+
+	void Awake()
+	{
+		myTransform = transform;
+	}
+
+	void Start()
+	{
+		GameObject stop = GameObject.FindGameObjectWithTag("Player");
+
+		target = stop.transform;
+
+		maxDistance = 7;
+	}
+
+	void FixedUpdate()
+	{
+		if (Vector3.Distance(target.position, myTransform.position) < maxDistance)
+		{
+			InvokeRepeating("Spawn", spawnDelay, spawnTime);
+		}
+	}
+
+	void Spawn()
+	{
+		// Instantiate a random enemy.
+		int ZombieIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[ZombieIndex], transform.position, transform.rotation);   
+	}
+}
